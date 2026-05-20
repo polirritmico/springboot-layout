0825797773446985615dfce52c69e960c2d98448
polirritmico <ebray187@gmail.com>
2026-05-20 18:03:47 -0400

    feat: add update layout tool

---
+++ misc/update-layout-files.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# e - script stops on error (return !=0)
+# u - error if undefined variable
+# o pipefail - script fails if one of piped command fails
+# x - output each line (debug)
+set -euo pipefail
+
+LAYOUT=../springboot-layout
+
+cp ${LAYOUT}/.env.example ./
+cp ${LAYOUT}/.gitattributes ./
+cp ${LAYOUT}/.gitignore ./
+cp ${LAYOUT}/Dockerfile ./
+cp ${LAYOUT}/compose.yml ./
+cp ${LAYOUT}/src/main/resources/* src/main/resources/
