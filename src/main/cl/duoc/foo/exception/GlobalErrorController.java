c787de33ed3815d2864f0c90bc11ca727f3707d0
polirritmico <ebray187@gmail.com>
2026-05-21 14:05:00 -0400

    feat(GlobalErrorController): add error log

--- src/main/cl/duoc/foo/exception/GlobalErrorController.java
+++ src/main/cl/duoc/foo/exception/GlobalErrorController.java
@@ -6,17 +6,25 @@
  */
 package cl.duoc.foo.exception;
 
-import cl.duoc.foo.exception.dto.ApiErrorResponse;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.webmvc.error.ErrorController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class GlobalErrorController implements ErrorController {
     @RequestMapping("/error")
-    public ResponseEntity<ApiErrorResponse> handleError(HttpServletResponse res) {
+    public ResponseEntity<Void> handleError(HttpServletRequest req, HttpServletResponse res) {
+        log.error(
+                "Container error - Status: {}, Path: {} | Message: {}",
+                res.getStatus(),
+                req.getAttribute(RequestDispatcher.ERROR_REQUEST_URI),
+                req.getAttribute(RequestDispatcher.ERROR_MESSAGE));
         return ResponseEntity.status(res.getStatus()).build();
     }
 }
